4f1b281a3bbab4f6c21f6308e46a3c525f484359
Yuri Rykov <yrykov@axsiumgroup.com>
2019-04-11 20:19:22 -0700

    Initial Commit

---
+++ WebMatrixExample/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace WebMatrixExample.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("UserDb", "Users", "Id", "UserName", autoCreateTables: true);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection form)
+        {
+            bool success = WebSecurity.Login(form["username"], form["password"], false);
+            if (success)
+            {
+                string returnUrl = Request.QueryString["ReturnUrl"];
+                if (returnUrl == null)
+                {
+                    Response.Redirect("~/home/index");
+                }
+                else
+                {
+                    Response.Redirect(returnUrl);
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            if (!WebSecurity.Initialized)
+            {
+                WebSecurity.InitializeDatabaseConnection("UserDb", "Users", "Id", "UserName", autoCreateTables: true);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(FormCollection form)
+        {
+            WebSecurity.CreateUserAndAccount(form["username"], form["password"], new { DisplayName = form["displayname"], Country = form["country"] });
+            Response.Redirect("~/account/login");
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            WebSecurity.Logout();
+            Response.Redirect("~/account/login");
+            return View();
+        }
+    }
+}
---
+++ WebMatrixExample/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace WebMatrixExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            if (!WebSecurity.IsAuthenticated)
+            {
+                Response.Redirect("~/account/login");
+            }
+            return View();
+        }
+    }
+}
